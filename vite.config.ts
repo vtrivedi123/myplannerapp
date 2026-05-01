4bf389eecf0b5045b2167a72a97abd7530ec5a90
Vishal Trivedi <85580927+vtrivedi123@users.noreply.github.com>
2026-04-30 23:31:21 -0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -221,7 +221,7 @@ export default defineConfig({
     emptyOutDir: true,
   },
   server: {
-    port: 3000,
+    port: 4000,
     strictPort: false, // Will find next available port if 3000 is busy
     host: true,
     allowedHosts: [
