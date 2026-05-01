c24c47b45c7d649197517d7e51dc51cd26a49d6f
Vishal Trivedi <85580927+vtrivedi123@users.noreply.github.com>
2026-05-01 13:43:00 -0400

    Add files via upload

--- client/src/components/ui/calendar.tsx
+++ client/src/components/ui/calendar.tsx
@@ -108,7 +108,7 @@ function Calendar({
         range_middle: cn("rounded-none", defaultClassNames.range_middle),
         range_end: cn("rounded-r-md bg-accent", defaultClassNames.range_end),
         today: cn(
-          "bg-accent text-accent-foreground rounded-md data-[selected=true]:rounded-none",
+          "text-foreground",
           defaultClassNames.today
         ),
         outside: cn(
